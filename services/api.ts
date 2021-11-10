d03cfd76498cf4bb514be2d177a8161e1ce670ff
Felipe <fellipefreiire3@gmail.com>
2021-11-10 14:35:52 -0300

    refresh token

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
-import { parseCookies } from 'nookies'
+import { parseCookies, setCookie } from 'nookies'
 
-const cookies = parseCookies()
+let cookies = parseCookies()
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -10,3 +10,35 @@ export const api = axios.create({
   }
 })
 
+api.interceptors.response.use(response => {
+  return response
+}, (error) => {
+  if (error.response.status === 401) {
+    if (error.response.data?.code === 'token.expired') {
+      cookies = parseCookies()
+
+      const { 'nextauth.refreshToken': refreshToken } = cookies
+
+      api.post('/refresh', {
+        refreshToken
+      }).then(response => {
+        const { token } = response.data
+
+        setCookie(undefined, 'nextauth.token', token, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        })
+
+        setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        })
+
+        api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+      })
+
+    } else {
+      // deslogar
+    }
+  }
+})
